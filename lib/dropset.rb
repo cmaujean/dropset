dbb07a027aaa63992048aa6347867298b5bd8eb1
Christopher Maujean <cmaujean@gmail.com>
2011-08-29 12:38:29 -0700

    changing the calculation to be more mathmatically correct, rather than using logic to correct the typecasting issues

--- lib/dropset.rb
+++ lib/dropset.rb
@@ -2,7 +2,8 @@ class Dropset
   
   def self.calc(n)
     ( n.kind_of? Fixnum and n > 0 ) or raise ArgumentError
-    (n * n / 2) + ( n / 2 ) + (n.odd? ? 1 : 0)
+    n = n.to_f
+    ((n * n / 2) + ( n / 2 )).to_i
   end
 
 end
